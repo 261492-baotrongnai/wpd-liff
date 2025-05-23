58f66746ad91cf74e41634e0fc901911ed5321e2
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2025-05-23 16:16:10 +0700

    refactor : remove unused MonthProgress and WeekProgress components

--- src/pages/progress/section-components/MonthProgress.vue
+++
@@ -1,9 +0,0 @@
-<template>Month</template>
-
-<script lang="ts">
-export default {
-  name: 'MonthProgress',
-}
-</script>
-
-<style scoped></style>
--- src/pages/progress/section-components/WeekProgress.vue
+++
@@ -1,9 +0,0 @@
-<template>Week</template>
-
-<script lang="ts">
-export default {
-  name: 'WeekProgress',
-}
-</script>
-
-<style></style>
